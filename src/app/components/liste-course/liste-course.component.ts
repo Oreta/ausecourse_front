fa9080e98cc650dbb9c2eb3ec9b4fa98b732277b
Vergriete Florian <florian.vergriete.etu@univ-lille.fr>
2018-12-08 00:12:22 +0100

    display list

--- src/app/components/liste-course/liste-course.component.ts
+++ src/app/components/liste-course/liste-course.component.ts
@@ -14,9 +14,13 @@ import {MatTableModule} from '@angular/material/table';
 })
 export class ListeCourseComponent implements OnInit {
 
-  private commande: Product[] = [
-    {id:"5" ,nom:"test",quantity:2,prix:32}
+  public commande: Product[] = [
+    {id:"5" ,nom:"test",quantity:2,prix:32},
+    {id:"7" ,nom:"te",quantity:2,prix:32},
+    {id:"32" ,nom:"testine",quantity:2,prix:32}
   ];
+
+  private produc:Product;
 
   constructor(
 	  	private productService: ProductService,
@@ -24,21 +28,25 @@ export class ListeCourseComponent implements OnInit {
 	    private http:Http,
 	    private route:ActivatedRoute) { }
 
-  onUpdateCourse(){
-    
+
+  onAddProduct(){
+    console.log(this.commande);
+    this.produc.id = "7";
+    this.produc.prix = 32;
+    this.commande.push(produc);
   }
 
-
   ngOnInit() {
-    this.productService.getProductList().subscribe(
-      res => {
-        console.log(res);
-      },
-      error => {
-        // console.log(error.text());
-        // let errorMessage = error.text();
-      }
-    );
+    console.log(this.commande);
+    // this.productService.getProductList().subscribe(
+    //   res => {
+    //     console.log(res);
+    //   },
+    //   error => {
+    //     // console.log(error.text());
+    //     // let errorMessage = error.text();
+    //   }
+    // );
   }
 
 }
